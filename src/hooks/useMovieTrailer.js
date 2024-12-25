0823ba36d6b8a7bdfbb5f342e33de6ac6035c7d2
Mikhil <mighilmurali@gmail.com>
2024-12-25 23:43:07 +0530

    added memoisation

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,11 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 
 const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
-
+    const trilerVideo = useSelector(store => store.movies?.trilerVideo);
     const getTrailerVideo = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US', API_OPTIONS);
         const json = await data.json();
@@ -16,7 +16,7 @@ const useMovieTrailer = (movieId) => {
     }
 
     useEffect(() => {
-        getTrailerVideo();
+        !trilerVideo && getTrailerVideo();
     }, [])
 }
 
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch();
+    const nowPlayingMovies = useSelector(store => store.movies?.nowPlayingMovies);
     const getNowPlayingMoviesList = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
         const json = await data.json();
@@ -12,7 +13,7 @@ const useNowPlayingMovies = () => {
     }
 
     useEffect(() => {
-        getNowPlayingMoviesList();
+        !nowPlayingMovies?.length && getNowPlayingMoviesList();
     }, [])
 }
 
